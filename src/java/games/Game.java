8bb8da3b4bf79cb05b920136e8f07adf1e34dc84
Tobias Ephron <tobiasephron@gmail.com>
2025-08-04 12:59:55 -0400

    Updating the file structure and adding a class for Games

---
+++ src/java/games/Game.java
@@ -0,0 +1,73 @@
+
+/**
+ * A general game class.
+ *
+ * @author Tobias Ephron
+ */
+
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public abstract class Game {
+
+    protected final String name;
+    protected final Timestamp endTime;
+    protected List<String> stats;
+    protected Map<String, Object> statValues;
+    protected boolean win;
+
+    public Game(
+            String name,
+            Timestamp endTime,
+            List<String> stats,
+            Map<String, Object> statValues,
+            boolean win) {
+        this.name = name;
+        this.endTime = endTime;
+        this.stats = stats;
+        
+        if (checkStatValues(statValues)) {}
+
+        this.win = win;
+    }
+
+    private boolean checkStatValues(Map<String, Object> map) {
+        return this.stats.size() == map.keySet().size() && this.stats.containsAll(map.keySet());
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Timestamp getEndTime() {
+        return this.endTime;
+    }
+
+    public List<String> getStats() {
+        return this.stats;
+    }
+
+    public Map<String, Object> getStatValues() {
+        return this.statValues;
+    }
+
+    public Map<String, Object> getStatsValues(List<String> keys) {
+        return this.statValues.entrySet()
+                .stream()
+                .filter(entry -> keys.contains(entry.getKey()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    public void setStatValue(String key, Object value) {
+        this.statValues.put(key, value);
+    }
+
+    /**
+     * Calculate the number of reps of a workout to do for the game.
+     *
+     * @return the number of reps
+     */
+    public abstract int calculateReps();
+}
