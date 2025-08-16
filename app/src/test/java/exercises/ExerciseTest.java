7fede9ce1c3804651cf2b9bd366a7aae992a025f
Tobias Ephron <tobiasephron@gmail.com>
2025-08-16 09:13:43 -0400

    Add ExerciseTest

---
+++ app/src/test/java/exercises/ExerciseTest.java
@@ -0,0 +1,37 @@
+/**
+ * Tests for the Exercise class
+ * 
+ * @author Tobias Ephron
+ */
+
+package test.java.exercises;
+
+import org.junit.jupiter.api.Test;
+
+import main.java.exercises.Exercise;
+import main.java.exercises.Exercise.ExerciseType;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ExerciseTest {
+    @Test
+    public void getsName() {
+        Exercise exercise = new Exercise("test_1", ExerciseType.REPS);
+
+        assertEquals("test_1", exercise.getName());
+    }
+
+    @Test
+    public void getsRepsType() {
+        Exercise exercise = new Exercise("test_1", ExerciseType.REPS);
+
+        assertEquals(ExerciseType.REPS, exercise.getType());
+    }
+
+    @Test
+    public void getsTimedType() {
+        Exercise exercise = new Exercise("test_2", ExerciseType.TIMED);
+
+        assertEquals(ExerciseType.TIMED, exercise.getType());
+    }
+}
